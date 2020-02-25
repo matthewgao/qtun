3784accce21b8bf57c3a76f1518dfc58cc7a0d09
gaoshen.gs <gaoshen.gs@alibaba-inc.com>
2020-02-25 13:33:59 +0800

    pprof

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// _ "net/http/pprof"
+	_ "net/http/pprof"
 	"runtime"
 	"strconv"
 
@@ -120,6 +120,6 @@ func pprof() {
 }
 
 func main() {
-	// pprof()
+	pprof()
 	Init()
 }
